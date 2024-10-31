eecc9a1a53828dddb4d301d0c4142b0e83d78110
economy937 <economy9895@naver.com>
2024-10-31 20:59:41 +0900

    [Update]Map
    무한맵 생성 추가

--- Assets/MapManager.cs
+++ Assets/MapManager.cs
@@ -1,44 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject[] mapPrefabs;
-    [SerializeField]
-    private int initialMapCount = 5;
+    public GameObject[] mapPrefabs; 
+    public int initialMapCount = 5; 
+    public float mapLength = 30f; 
 
-    private int prefabsLenth = 30;
-    private float spawnZ = 0f;
-
-    int random = 0;
-
-    public void SpwanMap() 
-    {
-        random = Random.Range(0, 2);
-        GameObject go;
-        go = Instantiate(mapPrefabs[random]); 
-        go.transform.SetParent(transform);
-        go.transform.position = Vector3.forward * spawnZ;
-        spawnZ += prefabsLenth;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision) 
-    {
-        if (collision.CompareTag("Player"))
-        {
-            Debug.Log("»ý¼º");
-        }
-
-    }
-
+    private Queue<GameObject> mapPool = new Queue<GameObject>(); 
+    private Vector3 nextMapPosition = Vector3.zero; 
     private void Start()
+    {
+        InitializeMapPool();
+    }
+
+    private void InitializeMapPool()
     {
         for (int i = 0; i < initialMapCount; i++)
         {
-            SpwanMap();
+            GameObject map = Instantiate(GetRandomMapPrefab(), nextMapPosition, Quaternion.identity);
+            mapPool.Enqueue(map);
+            nextMapPosition.z += mapLength; 
         }
-        
+    }
+
+    private GameObject GetRandomMapPrefab()
+    {
+        return mapPrefabs[Random.Range(0, mapPrefabs.Length)];
+    }
+
+    public void RepositionMap()
+    {
+        GameObject oldMap = mapPool.Dequeue();
+        oldMap.SetActive(false);
+
+        GameObject newMap = Instantiate(GetRandomMapPrefab(), nextMapPosition, Quaternion.identity);
+        newMap.SetActive(true);
+
+        nextMapPosition.z += mapLength;
+
+        Destroy(oldMap);
+        mapPool.Enqueue(newMap);
     }
 }
