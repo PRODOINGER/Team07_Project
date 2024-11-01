8c75ba8a09e276caa827475e448d5a4e314ddc11
jaund1ce <66049637+jaund1ce@users.noreply.github.com>
2024-11-01 15:03:17 +0900

    [add]sound
    사운드 추가 및 사운드 조절(배경,효과음 분리)기능 추가

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
-   public static SoundManager instance;
+    public AudioMixer mixer;
+    public AudioSource BgmSound;
+    public AudioClip[] BgmList;
+    public static SoundManager instance;
 
     private void Awake()
     {
@@ -12,6 +17,7 @@ public class SoundManager : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(instance);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
 
         else
@@ -20,13 +26,39 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        for (int i = 0; i < BgmList.Length; i++)
+        {
+            BackguoundSound(BgmList[i]);
+        }
+    }
+
+    public void BgmSoundVoiume(float val)
+    {
+        mixer.SetFloat("BgmSound", Mathf.Log10(val) * 20);
+    }
+    public void SfxSoundVoiume(float val)
+    {
+        mixer.SetFloat("SfxSound", Mathf.Log10(val) * 20);
+    }
+
     public void SFXPlay(string sfxName, AudioClip clip)
     {
         GameObject go = new GameObject(sfxName+"Sound");
         AudioSource audioSource = go.AddComponent<AudioSource>();
+        audioSource.outputAudioMixerGroup = mixer.FindMatchingGroups("SFX")[0];
         audioSource.clip = clip;
         audioSource.Play();
-
         Destroy(go, clip.length);
+    }
+
+    public void BackguoundSound(AudioClip clip)
+    {
+        BgmSound.outputAudioMixerGroup = mixer.FindMatchingGroups("BGM")[0];
+        BgmSound.clip = clip;
+        BgmSound.loop = true;
+        BgmSound.volume = 0.1f;
+        BgmSound.Play();
     }
 }
